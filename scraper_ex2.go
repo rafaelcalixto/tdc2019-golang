effb0f5b73b77c2da0e30306b332481e158d6bbf
Rafael Calixto <r.calixto@wisetrust.io>
2019-04-22 21:06:56 -0300

    almost completed

--- scraper_ex2.go
+++ scraper_ex2.go
@@ -6,67 +6,76 @@ import (
     "golang.org/x/net/html"
     "golang.org/x/net/html/atom"
     "strings"
+    "sync"
 )
 
 var (
     startURL  string
     linkList []string
+    wg sync.WaitGroup
 )
 
-func ProcessLinks() {
+func ProcessLinks(links []string) ([]string) {
     dict := map[string]bool{}
 
-    for _, link := range linkList {
+    for _, link := range links {
         link = strings.TrimSpace(link)
         if len(link) < 10 { continue }
         if link[:4] == "http" && link != startURL && link[:len(link)-1] != startURL  {
             dict[link] = true
         }
     }
-    linkList = linkList[:0]
+    links = links[:0]
     for k, _ := range dict {
-        linkList = append(linkList, k)
+        links = append(links, k)
     }
+    fmt.Println("Total links found:", len(links))
+    return links
 }
 
-func GetLinks(startURL string) {
-    ans, _ := http.Get(startURL)
-    defer ans.Body.Close()
-    tags := html.NewTokenizer(ans.Body)
-
+func GetLinks(t *html.Tokenizer, links []string) ([]string) {
     for {
-        _ = tags.Next()
-        token := tags.Token()
+        _ = t.Next()
+        token := t.Token()
 
         if token.Type == html.ErrorToken { break }
-
         if token.DataAtom == atom.A && token.Type == html.StartTagToken {
             for _, attr := range token.Attr {
-                if attr.Key == "href" { linkList = append(linkList, attr.Val) }
+                if attr.Key == "href" { links = append(links, attr.Val) }
             }
         }
     }
-    ProcessLinks()
+    return links
 }
 
-func CountLinks(listPath []string) {
-    var newLinksList []string
+func Scraper(c chan []string, url string) {
+    fmt.Println("going check -> " + url)
+    var linkList []string
+    defer wg.Done()
+    ans, _ := http.Get(url)
+    defer ans.Body.Close()
+    tags := html.NewTokenizer(ans.Body)
+    c <- ProcessLinks(GetLinks(tags, linkList))
+}
 
-    for _, link := range listPath {
-        go GetLinks(link)
+func CountLinks(links []string) {
+    newChan := make(chan []string, 30)
+    for _, l := range links {
+        wg.Add(1)
+        go Scraper(newChan, l)
     }
-}
-
-func UpdateValue(l *[]string) {
-    *l = linkList
-    linkList = linkList[:0]
+    wg.Wait()
+    close(newChan)
 }
 
 func main() {
     var startLinks []string
+    chanLinks := make(chan []string, 30)
     startURL = "https://panoramacrypto.com.br"
-    GetLinks(startURL)
-    UpdateValue(&startLinks)
-
+    wg.Add(1)
+    go Scraper(chanLinks, startURL)
+    wg.Wait()
+    close(chanLinks)
+    startLinks = <-chanLinks
     CountLinks(startLinks)
 }
